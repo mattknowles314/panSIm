443eac48ed725fb1ea88d98b7889136f598078f4
Matthew Knowles <mattknowles314@gmail.com>
2020-03-31 13:34:11 +0100

    Made some good progress on solving the equations, current plots seem a bit weird though

--- sirSolve.py
+++ sirSolve.py
@@ -1,34 +1,50 @@
 """
 We recall the 'SIR' equations
-    S' = -BSI -> succ
-    I' =BSI-cI -> infec
+    S' = -bSI/N -> succ
+    I' =bSI/N-cI -> infec
     R' = cI -> rem
 
-Where S is the susceptible population, I are those infected, and R is those removed (dead or recovered)
+Where S is the susceptible population, I are those infected, and R is those removed (dead or recovered) and N is the total poulation. 
 """
 
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import odeint
 
-# Constants, these will be imported from the GUI 
+#First we have the parameters for the model, these will be imported from the GUI 
+#Pop Stats:
+N = 1000 #This is the size of the population
+I0 = 1 #Initial number of infected people
+R0 = 0 #Initial number of removed people
+S0 = N-I0-R0 #Susceptible population initially
 
-S = 100
-I = 0
-R = 0
+#Virus Stats:
+b = 0.4 #Rate of infection
+c = 0.2 #Rate of recovery
+per = 150 #Infection period 
 
-B = 3.0
-c = 0.2
 
-# Solve the equations
+#Now we can make the model using the SIR equations 
+t = np.linspace(0,per,per) #Time grid
 
-def succ(S,I,R,B,c):
-        dSdt = -1*B*S*I
-        return dSdt
+def sireqs(v,t,N,b,c):
+    S,I,R = v #This puts the three values as a vector
+    dSdt = -1*b*S*I/N
+    dIdt = b*S*I/N - c*I 
+    dRdt = c*I
+    return dIdt,dRdt,dSdt
 
-t = np.linspace(0,20)
-S = odeint(succ, 100, t)
+#Because in sireqs we gave v=S,I,R as a vector, we do so for initial conditions
+v0 = S0,I0,R0 #Initial conditions we gave earlier but as a vector
 
-plt.plot(t,y)
+sirInt = odeint(sireqs, v0,t,args=(N,b,c))
+S,I,R = sirInt.T
+plt.plot(t,S/1000, 'b')
+plt.plot(t,I/1000, 'g')
+plt.plot(t,R/1000, 'r')
 plt.show()
+
+
+
+
 
