c96f87373a7ec4a23d39dd2b902092cde2de6b6b
Matthew Knowles <mattknowles314@gmail.com>
2020-04-01 21:43:02 +0100

    Now solves SIR equations properly, code currently still based on SciPy example

--- sirSolve.py
+++ sirSolve.py
@@ -13,35 +13,43 @@ from scipy.integrate import odeint
 
 #First we have the parameters for the model, these will be imported from the GUI 
 #Pop Stats:
-N = 1000 #This is the size of the population
+N = 5000 #This is the size of the population
 I0 = 1 #Initial number of infected people
 R0 = 0 #Initial number of removed people
 S0 = N-I0-R0 #Susceptible population initially
 
 #Virus Stats:
-b = 0.2#Rate of infection
+b = 0.4 #Rate of infection
 c = 1./10 #Rate of recovery
-per = 240 #Infection period 
+per = 500 #Infection period 
 
 
 #Now we can make the model using the SIR equations 
-t = np.linspace(0,per,per) #Time grid
+t = np.linspace(0,300,300) #Time grid
 
-def sireqs(v,t,N,b,c):
-    S,I,R = v #This puts the three values as a vector
-    dSdt = -1*b*S*I/N
+def sireqs(y,t,N,b,c):
+    S,I,R = y #This puts the three values as a vector
+    dSdt = -b*S*I/N
     dIdt = b*S*I/N - c*I 
     dRdt = c*I
-    return dIdt,dRdt,dSdt
+    return dSdt, dIdt, dRdt
 
 #Because in sireqs we gave v=S,I,R as a vector, we do so for initial conditions
-v0 = S0,I0,R0 #Initial conditions we gave earlier but as a vector
-
-sirInt = odeint(sireqs, v0,t,args=(N,b,c))
+y0 = S0,I0,R0 #Initial conditions we gave earlier but as a vector
+sirInt = odeint(sireqs, y0,t,args=(N,b,c))
 S,I,R = sirInt.T
-plt.plot(t,S/1000, 'b')
-plt.plot(t,I/1000, 'g')
-plt.plot(t,R/1000, 'r')
+
+
+
+plt.plot(t,S/1000, 'b', alpha=0.5, lw=2,label = 'Susceptible')
+plt.plot(t,I/1000, 'r', alpha=0.5, lw=2,label = 'Infected') 
+plt.plot(t,R/1000, 'g', alpha=0.5, lw=2,label = 'Removed')
+plt.xlabel('Time/days')
+plt.ylabel('Number of each parameter /1000s')
+legend =plt.legend()
+
+
+
 plt.show()
 
 
